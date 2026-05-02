cac58db6e051eaf45f6eba1582859648d3628083
awakening <awakeningjuniorgroup@gmail.com>
2026-05-02 18:47:12 +0100

    fix: resolution des erreurs de build et fichiers manquants

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -6,16 +6,13 @@ import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
 import star_icon from "./star_icon.svg";
 import star_dull_icon from "./star_dull_icon.svg";
 import cart_icon from "./cart_icon.svg";
-import nav_cart_icon from "./nav_cart_icon.svg";
 import add_icon from "./add_icon.svg";
 import refresh_icon from "./refresh_icon.svg";
 import product_list_icon from "./product_list_icon.svg";
 import order_icon from "./order_icon.svg";
 import upload_area from "./upload_area.png";
-import profile_icon from "./profile_icon.png";
 import menu_icon from "./menu_icon.svg";
 import delivery_truck_icon from "./delivery_truck_icon.svg";
-import leaf_icon from "./leaf_icon.svg";
 import coin_icon from "./coin_icon.svg";
 import box_icon from "./box_icon.svg";
 import trust_icon from "./trust_icon.svg";
@@ -84,16 +81,13 @@ export const assets = {
   star_icon,
   star_dull_icon,
   cart_icon,
-  nav_cart_icon,
   add_icon,
   refresh_icon,
   product_list_icon,
   order_icon,
   upload_area,
-  profile_icon,
   menu_icon,
   delivery_truck_icon,
-  leaf_icon,
   coin_icon,
   trust_icon,
   black_arrow_icon,
